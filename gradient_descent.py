8382faccfb390aedfdabc849ce722a9ced6ffd53
Aref <rf.gholami@yahoo.com>
2019-05-01 00:44:21 +0430

    fix bugs

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+from sigmoid import sigmoid
+
 
 def gradient_descent(X, y, theta, learning_rate, m):
-    h = np.dot(X, theta)
-    grad = (np.dot(X.T, (h - y)))
+    h = sigmoid(np.dot(X, theta))
+    # grad = (np.dot(X.T, (h - y)))
+    grad = np.sum(np.multiply(X, (h - y)),axis=0)
+
+
     grad = np.multiply((learning_rate / m), grad)
-    return grad
+    return grad.reshape((3, 1))
--- main.py
+++ main.py
@@ -19,12 +19,13 @@ plt.show()
 X = add_x0(X)
 m = X.shape[0]
 n = X.shape[1]
-learning_rate = .01
+learning_rate = .3
 theta = np.zeros((n, 1))
+max_iter = 800
 
-his = np.zeros((1000, 1))
+his = np.zeros((max_iter, 1))
 
-for i in range(1000):
+for i in range(max_iter):
 
     cost = compute_cost(X, y, theta)
     grad = gradient_descent(X, y, theta, learning_rate, m)
@@ -32,7 +33,6 @@ for i in range(1000):
 
     his[i, :] = cost
 
-    # print (add_to_learning_rate)
     if i % 100 == 0:
         print (str(i) + "--" + str(cost))
 
@@ -48,33 +48,27 @@ plt.show()
 
 plt.scatter(X[:, 1], X[:, 2], c=y, s=50, cmap=plt.cm.Spectral)
 
-old_theta = np.array([[-25.161272],
+best_theta = np.array([[-25.161272],
                       [0.206233],
                       [0.201470]], dtype=float)
 
 xa1 = 0
-xb1 = -(old_theta[0] + xa1 * old_theta[1]) / old_theta[2]
+xb1 = -(best_theta[0] + xa1 * best_theta[1]) / best_theta[2]
 xb2 = 0
-xa2 = -(old_theta[0] + xb2 * old_theta[2]) / old_theta[1]
+xa2 = -(best_theta[0] + xb2 * best_theta[2]) / best_theta[1]
 
 xa1 = (xa1 - mu) / sigma
 xb1 = (xb1 - mu) / sigma
 xa2 = (xa2 - mu) / sigma
 xb2 = (xb2 - mu) / sigma
 
-plt.plot([xa1, xa2], [xb1, xb2], 'k-', lw=1, color='blue', label='old_theta')
+plt.plot([xa1, xa2], [xb1, xb2], 'k-', lw=1, color='blue', label='best_theta')
 
-xa1 = 0
+xa1 = -3
 xb1 = -(theta[0] + xa1 * theta[1]) / theta[2]
 
-xb1 = -(xb1 - mu) / sigma - theta[0]
-xa1 = (xa1 - mu) / sigma
-
-xb2 = 0
+xb2 = -3
 xa2 = -(theta[0] + xb2 * theta[2]) / theta[1]
-
-xa2 = -(xa2 - mu) / sigma - theta[0]
-xb2 = (xb2 - mu) / sigma
 
 plt.plot([xa1, xa2], [xb1, xb2], 'k-', lw=1, color='red', label='theta')
 plt.legend(loc='upper center', shadow=True)
@@ -96,10 +90,13 @@ x = np.array([
     [99.31500880510394, 68.77540947206617],
     [55.34001756003703, 64.9319380069486],
     [74.77589300092767, 89.52981289513276],
-
+    [10.77589300092767, 10.52981289513276],
+    [100.77589300092767, 100.52981289513276],
+    [-0.77589300092767, -0.52981289513276],
 ], dtype=float)
-predicted1 = str((predict(x, old_theta, 0, 1) > .5))
-predicted2 = str((predict(x, theta, 0, 1) > .5))
 
-print("predicted value for " + str(x) + " with old_theta = " + predicted1)
-print("predicted value for " + str(x) + " with theta = " + predicted2)
+predicted1 = np.array(predict(x, best_theta, 0, 1) > .5, dtype=int)
+predicted2 = np.array(predict(x, theta, mu, sigma) > .5, dtype=int)
+
+print("predicted value  with best_theta = " + str(predicted1))
+print("predicted value  with theta = " + str(predicted2))
